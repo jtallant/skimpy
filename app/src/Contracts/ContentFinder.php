922ef087dfbc8d187da6875c88bdfc4ffcbbdf23
Justin Tallant <jtallant07@gmail.com>
2014-11-06 01:22:14 -0600

    not using this interface

--- app/src/Contracts/ContentFinder.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Skimpy\Contracts;
-
-# Should I bother with this?
-interface ContentFinder
-{
-    public function findByName($name);
-
-    public function findPostsContaining($string);
-}
