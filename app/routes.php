36b30c76befe61a172b06aa2d6e7dca16200d462
Justin Tallant <jtallant07@gmail.com>
2014-11-23 02:01:52 -0600

    remove space

--- app/routes.php
+++ app/routes.php
@@ -3,7 +3,7 @@
  * Routes
  */
 
- use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
