16b4b53dc3c39dee789a448183e74ca2852f9e7b
Justin Tallant <jtallant07@gmail.com>
2014-12-17 13:28:29 -0600

    namespace not used

--- app/routes.php
+++ app/routes.php
@@ -6,7 +6,6 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
-use AgnosticPhp\Support\Collection;
 
 /**
  * Home page
