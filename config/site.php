fd12d4137139e75f88c2b3c9363153ea5caddbc7
Justin Tallant <jtallant07@gmail.com>
2018-12-12 08:08:56 -0600

    use php for config

---
+++ config/site.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'site.title'            => 'Skimpy',
+    'site.tagline'          => 'Site tagline',
+    'site.author'           => 'Justin Tallant',
+    'site.ga_tracking_id'   => 'UA-XXXXXXXX-1',
+    'site.timezone'         => 'America/Chicago',
+    'site.date_format'      => 'F jS, Y',
+    'site.meta_description' => 'Site meta description goes here',
+    'debug'                 => '%env.DEBUG',
+    'build_key'             => null,
+    'auto_rebuild'          => '$end.AUTO_REBUILD',
+    'skimpy.db.file'        => __DIR__.'/../db/skimpy.sqlite',
+    'http_cache.enabled'    => false,
+];
